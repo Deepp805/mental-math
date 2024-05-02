f31ec5150e6c825bfa920e6db4edac683160766f
Deepp805 <deeppatel1944@gmail.com>
2024-05-01 22:17:20 -0400

    adding user works when registering for the first time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import MathChallenge from './MathChallenge'
-import ConfigurationPage from './ConfigurationPage'
 import AppRouter from './AppRouter'
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, UserButton, useAuth, useUser } from "@clerk/clerk-react";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 function App() {
+
+  const { isSignedIn, user, isLoaded } = useUser();
+  if (isSignedIn) {
+    console.log("This is the user Id:" + user.id);
+    console.log("This is their email address: " + user.primaryEmailAddress);
+    axios.post('/register', {
+      user_id: user.id,
+      user_email: user.primaryEmailAddress?.toString()
+    })
+  }
 
   return (
     <>

0a821e969c45485ba1e19039ffb8919c65e1a782
Deepp805 <deeppatel1944@gmail.com>
2024-05-05 17:22:06 -0400

    changing base path back to root

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mental-math/',  // Set the base URL for all assets
-})
+  base: '/',
+});

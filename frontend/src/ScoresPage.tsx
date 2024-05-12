b00e1c9aa3076b96383e499c50c13a4af51a1f08
Deepp805 <deeppatel1944@gmail.com>
2024-05-12 17:10:06 -0400

    removed Link import for vercel purposes

--- frontend/src/ScoresPage.tsx
+++ frontend/src/ScoresPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { getScores } from './api';
 import { format } from 'date-fns';
 import {Button} from '@chakra-ui/react';
